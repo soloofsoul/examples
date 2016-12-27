7116292cede8c23ef29f84d91bdc220a26282cc2
Grygorii Gavaleshko <grygoriy@gavaleshko.org.ua>
2016-12-27 12:39:21 +0100

    Cleaned code

--- patterns/adapter/js/src/main.js
+++ patterns/adapter/js/src/main.js
@@ -1,8 +1,5 @@
 // Adapter design pattern
 CustomQueryLibrary = function(){};
-CustomQueryLibrary.prototype.doSomething = function(){
-    console.log('NativeQuery.doSomething method');
-};
 CustomQueryLibrary.prototype.query = function(selector, context){
     context = context || document;
     return context.querySelectorAll(selector);
