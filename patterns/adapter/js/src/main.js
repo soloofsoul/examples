04320b0288c3e46f609b18e02701cada6cf0d4e7
Grygorii Gavaleshko <grygoriy@gavaleshko.org.ua>
2016-12-27 12:09:52 +0100

    Adde small updates

--- patterns/adapter/js/src/main.js
+++ patterns/adapter/js/src/main.js
@@ -1,18 +1,32 @@
 // Adapter design pattern
-NativeQuery = function(){};
-NativeQuery.prototype.query = function(selector, context){
+CustomQueryLibrary = function(){};
+CustomQueryLibrary.prototype.doSomething = function(){
+    console.log('NativeQuery.doSomething method');
+};
+CustomQueryLibrary.prototype.query = function(selector, context){
     context = context || document;
     return context.querySelectorAll(selector);
 };
 
-var testObj = {
-    NQObj: new NativeQuery()
+LibraryAdapter = function(lib){ this.lib = lib; };
+LibraryAdapter.prototype.query = function(selector, context) {
+    context = context || document;
+    return this.lib.query(selector, context);
 };
 
+var testObj = {
+    NQObj: new CustomQueryLibrary()
+};
+
+testObj.LAobj= new LibraryAdapter(testObj.NQObj);
 
 
 var contentDivEls = testObj.NQObj.query('#content'),
-    contentDivSpanEls = testObj.NQObj.query('span', contentDivEls[0]);
+    contentDivSpanEls = testObj.NQObj.query('span', contentDivEls[0]),
+    contentDivEls1 = testObj.LAobj.query('#content'),
+    contentDivSpanEls1 = testObj.LAobj.query('span', contentDivEls[0]);
 
 console.log('contentDivEls %O: ', contentDivEls[0]);
 console.log('contentDivSpanEls %O: ', contentDivSpanEls[0]);
+console.log('contentDivEls1 %O: ', contentDivEls1[0]);
+console.log('contentDivSpanEls1 %O: ', contentDivSpanEls1[0]);
